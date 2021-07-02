db038769a371a2b3400bfd363d579d95d49648e4
Alexander Bedoya Espinel <alex22122020@gmail.com>
2021-07-01 20:02:48 -0500

    Agrego los archivos del desafio

---
+++ archivo.js
@@ -0,0 +1,21 @@
+function ingreso(){
+    let numero = parseInt(prompt("ingrese un numero"));
+}
+
+function paroimpar(){ 
+    if(numero%2==0){
+        alert("es par");
+    } else {
+        alert("es impar");
+    }
+}
+
+function resultado(){
+    alert("el resultado es " + numero);
+}
+    
+// Aqui se encuentran las tres funciones si no que me esta quedando complicado enlazarlo con el html ya que no se como
+//llamar todas las funciones para que me lo lea el index. 
+
+
+
